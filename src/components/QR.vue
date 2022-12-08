7d15ca9c66619097fa118a70d665e4792c0d97aa
Lyserith <Lyserith@pm.me>
2022-12-08 17:24:14 +0100

    update

--- src/components/QR.vue
+++ src/components/QR.vue
@@ -1,12 +1,5 @@
-<style css>
-    img {
-        image-rendering: pixelated;
-        image-rendering: -moz-crisp-edges;
-        image-rendering: crisp-edges;
-    }
-</style>
 <template>
-    <vue-qr :dotScale=.9 backgroundColor="#00000000" colorLight="#00000000" :text="'http://app.pea.cash/#/wallet?output=' + public" :size=512 :correctLevel=3></vue-qr>
+    <vue-qr :dotScale=1 backgroundColor="#00000000" colorLight="#00000000" :text="'http://app.pea.cash/#/wallet?output=' + public" :size=1024 :correctLevel=0></vue-qr>
 </template>
 <script>
 import VueQr from 'vue-qr/src/packages/vue-qr.vue'
