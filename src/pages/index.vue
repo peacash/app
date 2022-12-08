864b815d60a31897ebb780b0f6a0d6488a3e2406
Lyserith <Lyserith@pm.me>
2022-12-08 20:09:35 +0100

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,15 +7,15 @@
 	}
 </style>
 <template>
-	<Endpoint />
-	<div class="flex flex-col gap-2 justify-center">
+	<div class="flex flex-col justify-between" style="min-height: calc(100% - 8rem);">
+		<Endpoint />
 		<Description>
-			<div class="flex justify-center gap-2 my-40">
+			<div class="flex justify-center gap-4 my-12">
 				<router-link class="link" to="/explorer">
-					<Button class="text-lg px-10 py-5">Explorer</Button>
+					<Button class="text-lg sm:text-lg md:text-lg w-40 md:w-60 h-16">Explorer</Button>
 				</router-link>
 				<router-link class="link" to="/wallet">
-					<Button class="text-lg px-10 py-5">Wallet</Button>
+					<Button class="text-lg sm:text-lg md:text-lg w-40 md:w-60 h-16">Wallet</Button>
 				</router-link>
 			</div>
 		</Description>
@@ -23,55 +23,8 @@
 </template>
 <script>
 export default {
-    data() {
-		return {
-			dynamic: null,
-			trusted: null,
-			sync: null,
-			info: null,
-			interval: null,
-			timeout: false,
-			https: window.location.protocol === "https:",
-			host: window.location.host,
-			endpoint: null,
-			shorten_public_key: true
-		}
-	},
     mounted() {
-		document.title = "Explorer - Pea";
-		this.loop();
-		setTimeout(() => {
-			this.timeout = true
-		}, 1000)
+		document.title = "Pea";
     },
-	unmounted() {
-		clearInterval(this.interval)
-	},
-	methods: {
-		loop() {
-			this.fetchData();
-			this.interval = setInterval(() => {
-				this.fetchData()
-			}, 3000);
-		},
-		shorten(string) {
-			return string.slice(0, 12) + "..." + string.slice(-8)
-		},
-		fetchData() {
-			this.endpoint = window.localStorage.getItem("endpoint");
-			fetch(this.endpoint + "/dynamic").then(res => res.json()).then(data => {
-				this.dynamic = data
-			})
-			fetch(this.endpoint + "/trusted").then(res => res.json()).then(data => {
-				this.trusted = data
-			})
-			fetch(this.endpoint + "/sync").then(res => res.json()).then(data => {
-				this.sync = data
-			})
-			fetch(this.endpoint + "/info").then(res => res.json()).then(data => {
-				this.info = data
-			})
-		}
-	},
 }
 </script>
