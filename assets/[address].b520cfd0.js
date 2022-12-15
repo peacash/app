cdaaab349e6a3662662fa78fff799f0c36a795ae
Lyserith <Lyserith@pm.me>
2022-12-15 14:20:46 +0100

    Updates

---
+++ assets/[address].b520cfd0.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./Address.5284b6b2.js";import{_ as r}from"./index.579d6670.js";import{o,e as t}from"./vendor.c2e542a0.js";const d={mounted(){document.title=this.$route.params.address+" - Address - Explorer - Pea"}};function a(n,_,c,p,m,i){const e=s;return o(),t(e,{addres:this.$route.params.address},null,8,["addres"])}var $=r(d,[["render",a]]);export{$ as default};
